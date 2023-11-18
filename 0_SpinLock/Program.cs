0e39852ccb91b5e577b44a9b61aaad82c2207b91
Grigor Khachatryan <grigor91khachatryan@gmail.com>
2023-11-18 11:41:45 +0400

    Add SpinLockexample

---
+++ 0_SpinLock/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _0_SpinLock
+{
+    internal class Program
+    {
+        class MyClass
+        {
+
+            SpinLock spLock = new SpinLock();
+            public void Process()
+            {
+                bool lockTaken = false;
+
+                spLock.Enter(ref lockTaken);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}  start");
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}  end");
+                spLock.Exit();
+
+            }
+        }
+        public static void Main()
+        {
+            MyClass ob = new MyClass();
+            Thread th = new Thread(new ThreadStart(ob.Process));
+            Thread th1 = new Thread(new ThreadStart(ob.Process));
+            Thread th2 = new Thread(new ThreadStart(ob.Process));
+            Thread th3 = new Thread(new ThreadStart(ob.Process));
+            th.Start();
+            th1.Start();
+            th3.Start();
+            th2.Start();
+            Console.Read();
+        }
+    }
+}
